bb175503b2fc37d02c6440d1ca2bb87d34cf985e
Lily <sh32321@gmail.com>
2024-08-27 10:54:33 -0400

    updated resume

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <li>Home</li>
       </Link>
       <Link
-        to='https://drive.google.com/file/d/1BpnHY4uttZ9b887cKcFUo_7qI-RVG2Ja/view?usp=sharing'
+        to='https://drive.google.com/file/d/1m50i9y3SHtqe2hwpWa9xjKAsNhZSW7xN/view?usp=sharing'
         target='_blank'>
         <li>Resume</li>
       </Link>
